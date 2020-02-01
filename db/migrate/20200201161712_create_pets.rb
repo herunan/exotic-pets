b2c812a062074cf70b2ec989bb1ac75c11fc082c
Hernan Romero <hernrome@gmail.com>
2020-02-01 16:29:56 +0000

    Created models with validations and associations. We can move on

---
+++ db/migrate/20200201161712_create_pets.rb
@@ -0,0 +1,15 @@
+class CreatePets < ActiveRecord::Migration[5.2]
+  def change
+    create_table :pets do |t|
+      t.string :name
+      t.string :species
+      t.text :description
+      t.string :address
+      t.integer :longitude
+      t.integer :latitude
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
