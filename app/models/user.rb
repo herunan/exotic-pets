fb58922b0e8a5c77021d52e0d19d775cb90a8f7a
Pablo Baldini <pablo.baldini@gmail.com>
2020-02-04 20:59:46 +0000

    Authorization to create and update pets. Everyone can see all pets and everyone can see a specific pet

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,4 @@ class User < ApplicationRecord
   has_many :pets, dependent: :destroy
   has_many :bookings, dependent: :destroy
   has_many :reviews, through: :bookings, dependent: :destroy
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :phone_number, presence: true
 end
