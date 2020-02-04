4b3b5c8af1d2dcbec1933a9d91288c6891318b0a
Pablo Baldini <pablo.baldini@gmail.com>
2020-02-04 21:03:43 +0000

    Authorization to create and update pets. Everyone can see all pets and everyone can see a specific pet

---
+++ app/policies/pet_policy.rb
@@ -0,0 +1,19 @@
+  class PetPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def show?
+    true # Anyone can see a pet
+  end
+
+  def create?
+    true
+  end
+
+  def update?
+    record.user == user
+  end
+end
