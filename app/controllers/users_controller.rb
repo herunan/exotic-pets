91e39aa3f7af0a9ae89af1473ad982200f3c11e3
Hernan Romero <hernrome@gmail.com>
2020-02-08 21:03:55 +0000

    Fixed pets create policy and controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,6 +27,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :phone_number, :photo)
+    params.require(:user).permit(:first_name, :last_name, :bio, :phone_number, :photo)
   end
 end
