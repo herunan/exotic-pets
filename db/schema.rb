2711ba9acc24db9cb91133d1932d95c75bf623c2
Pablo Baldini <pablo.baldini@gmail.com>
2020-02-08 13:53:31 +0000

    Carousel included in show view

--- db/schema.rb
+++ db/schema.rb
@@ -62,6 +62,15 @@ ActiveRecord::Schema.define(version: 2020_02_06_193757) do
     t.index ["user_id"], name: "index_pets_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.integer "rating"
+    t.bigint "booking_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["booking_id"], name: "index_reviews_on_booking_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -81,4 +90,5 @@ ActiveRecord::Schema.define(version: 2020_02_06_193757) do
   add_foreign_key "bookings", "pets"
   add_foreign_key "bookings", "users"
   add_foreign_key "pets", "users"
+  add_foreign_key "reviews", "bookings"
 end
