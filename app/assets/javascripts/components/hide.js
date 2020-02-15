f5b45855f78934fb69dde62a48651c7a528ae747
Kwame Dalrymple <neokwame@tutanota.com>
2020-02-15 13:30:26 +0000

    hide hero section on search WIP

---
+++ app/assets/javascripts/components/hide.js
@@ -0,0 +1,8 @@
+const form = document.querySelector(".simple_form");
+const heroSection = document.querySelector(".hero");
+
+ form.addEventListener("submit", (event) => {
+  heroSection.style.display = 'none';
+  // heroSection.classList.remove("hero");
+  // heroSection.classList.add("disabled");
+});
